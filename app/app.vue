bc73f1d1fdd1ae3dbbb8ceb2567fe3ab57783ab5
vstupitsyn <vstupitsyn@ipg.ru>
2026-04-17 14:32:19 +0300

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: 'ru'
+  },
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ]
+})
+
+useSeoMeta({
+  titleTemplate: title => title ? `${title} · Автолифт Про` : 'Автолифт Про',
+  ogSiteName: 'Автолифт Про',
+  twitterCard: 'summary_large_image'
+})
+</script>
+
+<template>
+  <UApp>
+    <NuxtPage />
+  </UApp>
+</template>
---
+++ app/pages/test.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <UPageSection>
+    <h1>Test</h1>
+  </UPageSection>
+</template>
+
+<style scoped>
+  h1 {
+    color: rgba(234, 0, 0, 0.7);
+  }
+</style>
